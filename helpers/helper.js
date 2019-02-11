80a6c9cec65afd83e00fcb64e26ab044b57213ed
gal <gcatabi@gmail.com>
2019-02-11 19:53:31 +0200

    check git commands

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,34 +1,7 @@
 const fs = require('fs');
 const path = require('path');
-// const getNewId = (array) => {
-//     if (array.length > 0) {
-//         return array[array.length - 1].id + 1
-//     } else {
-//         return 1
-//     }
-// }
-// const newDate = () => new Date().toString()
-// function mustBeInArray(array, id) {
-//     return new Promise((resolve, reject) => {
-//         const row = array.find(r => r.id == id)
-//         if (!row) {
-//             reject({
-//                 message: 'ID is not good',
-//                 status: 404
-//             })
-//         }
-//         resolve(row)
-//     })
-// }
 
-function checkFields(req, res, next) {
-    const { title, content, tags } = req.body
-    if (title && content && tags) {
-        next()
-    } else {
-        res.status(400).json({ message: 'fields are not good' })
-    }
-}
+
 
 function setJson(filename, content) {
     fs.writeFileSync(path.join(__dirname, `${filename}.json`) , JSON.stringify(content), 'utf8', (err) => {
@@ -43,11 +16,6 @@ function getJson(filename){
     return JSON.parse(content);  
 }
 
-module.exports = {
-    // getNewId,
-    // newDate,
-    // mustBeInArray,
-    checkFields,
-    setJson,
-    getJson
-}
+
+
+module.exports = { setJson, getJson }
--- models/students.js
+++ models/students.js
@@ -1,58 +1,79 @@
-const {setJson,getJson} =  require("../helpers/helper");
+const { setJson, getJson } = require("../helpers/helper");
 
 class Students {
 
-    constructor(){
+    constructor() {
         this.filenameStudents = '../data/students';
     }
 
-    getData(){
+    getData() {
         return getJson(this.filenameStudents);
     }
 
-    getStudents(req, res, next){
+    getStudents(req, res, next) {
         res.send(JSON.stringify(this.getData()));
     }
 
-    
-
-    setData(data){
-         setJson(this.filenameStudents,data);
+    setData(data) {
+        setJson(this.filenameStudents, data);
     }
 
-    // setStudents(req, res, next){
-    //     res.send(JSON.stringify(this.setData()));
-    // }    
+    setStudents(req, res, next) {
+        res.send(JSON.stringify(this.setData()));
+    }
 
-
-    addStudent(req, res, next){
-        // function addCourseToStudent(studentName,course){
-            var newStudent = this.getData(this.filenameStudents);
-            newStudent.push({
-                id_student : req.body.id_student,
-                id_course : req.body.id_course,
-                id_course : req.body.score
-
-            });
-
-
-            // const students = this.getData();
-            console.log(students)
-            for(let student of students){
-                if(student.id == studentName){
-                    if(student.courses.indexOf(course) == -1){
-                        student.courses.push(course)
-                    }
-                    break;
+    addStudent(req, res, next) {
+        const allStudents = this.getData();
+        const students = req.body;
+        const newStudent = {
+            id_student: students.id_student,
+            id_course: {
+                [students.id_course]: {
+                    score: students.score
                 }
+            },
+        }
+        for (let student of students) {
+            if (!students[newStudent.id_student]) {
+                students[student.id_student] = newStudent;
             }
-
-            var setNewStudent = this.setData(newCourse);       
-            res.send(setNewStudent);
+            allStudents.push(students[student.id_student]);
+            this.setData(allStudents);
+            this.setData(student);
+            res.send({ "sucess": true });
+            return;
+        }
+        res.send({ "faild": true });
     }
-    
 
-    OutstandingStudents(){
+    // addStudent(req, res, next){
+    //     // function addCourseToStudent(studentName,course){
+    //         var newStudent = this.getData(this.filenameStudents);
+    //         newStudent.push({
+    //             id_student : req.body.id_student,
+    //             id_course : req.body.id_course,
+    //             id_course : req.body.score
+
+    //         });
+
+
+    //         // const students = this.getData();
+    //         console.log(students)
+    //         for(let student of students){
+    //             if(student.id == studentName){
+    //                 if(student.courses.indexOf(course) == -1){
+    //                     student.courses.push(course)
+    //                 }
+    //                 break;
+    //             }
+    //         }
+
+    //         var setNewStudent = this.setData(newCourse);       
+    //         res.send(setNewStudent);
+    // }
+
+
+    OutstandingStudents() {
         var allList = this.getData();
         // var value = allList.
         Object.values(allList.id_student)
@@ -77,23 +98,66 @@ class Students {
 
     }
 
-    insertScoreForCourse (req, res, next){
+    insertScoreForCourse(req, res, next) {
         const students = this.getData();
-        const {id_course, id_student, score} = req.body;
-        for(let student of students){
-            if(student.id_student == id_student){
-                 if(student.id_course[id_course]){
-                    student.id_course[id_course].score = score;
-                    this.setData(students); 
-                    res.send({"sucess":true});
+        const { id_course, id_student, score } = req.body;
+        console.log(req.body);
+        for (let student of students) {
+            if (student.id_student == id_student) {
+                if (student.id_course[id_course]) {
+                    student.id_course[id_course].score = parseInt(score);
+                    // console.log(student.id_course)
+                    this.setData(students);
+                    res.send({ "sucess": true });
                     return;
 
-                 }
+                }
                 break;
             }
         }
-        res.send({"faild":true});
-        
+        res.send({ "faild": true });
+
+    }
+
+    average(id_course) {
+        var total = 0;
+
+        for (let courseKey in id_course) {
+            total += id_course[courseKey].score;
+        }
+        var numberOfKeys = Object.keys(id_course).length;
+        var avg = total / numberOfKeys;
+
+        return avg;
+    }
+
+    allStudentsAbove90Average(req, res, next) {
+        const students = this.getData();
+        var above90Average = []
+        for (let student of students) {
+            if (this.average(student.id_course) > 90) {
+                above90Average.push(student.id_student)
+            }
+        }
+        res.send(above90Average);
+    }
+
+    largestAverage() {
+        const students = this.getData();
+        var largestAverageStudent = [];
+        var highestScore = 0;
+        for (let student of students) {
+            var OutstandingStudents = this.average(student.id_course);
+            if (OutstandingStudents > highestScore) {
+                highestScore = OutstandingStudents;
+                OutstandingStudents = sutdent.id_student
+            }
+
+            largestAverageStudent.push(OutstandingStudents)
+
+        }
+        res.send(largestAverageStudent);
+
     }
 
 }
@@ -102,15 +166,3 @@ class Students {
 module.exports = Students;
 
 
-// function avrage(course_id){
-//     for course_id
-// }
-
-
-
-// var allsstudentsAbove90 = []
-// let student in students{
-//     if(avrage(student.course_id) > 90){
-//         allsstudentsAbove90.push(student)
-//     }
-// }
